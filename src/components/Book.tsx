caeee5e2319354bcd5832fab3776a8b48df9c164
Martin Winther <martin.winther@protonmail.com>
2024-03-27 09:37:40 +0100

    add icons

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { FaPen } from 'react-icons/fa'
 const Book = ({ book }) => {
   const [showFullDescription, setShowFullDescription] = useState(false)
 
@@ -30,7 +31,7 @@ const Book = ({ book }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaPen className="inline text-lg mb-1 mr-1"></FaPen>
             {book.author}
           </div>
           <a
