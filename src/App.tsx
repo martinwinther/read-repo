d85b31c538bfdd71a7426bb19349c98da5925718
Martin Winther <martin.winther@protonmail.com>
2024-04-16 11:45:04 +0200

    add add book page

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import HomePage from './pages/HomePage'
 import BooksPage from './pages/BooksPage'
 import NotFoundPage from './pages/NotFoundPage'
 import BookPage, { bookLoader } from './components/BookPage'
+import AddBookPage from './pages/AddBookPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/books" element={<BooksPage />} />
+      <Route path="/add-book" element={<AddBookPage />} />
       <Route path="/books/:id" element={<BookPage />} loader={bookLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
