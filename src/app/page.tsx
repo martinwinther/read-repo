bde218c9f842d25dc20381bf001b248d7ae59366
Martin Winther <martin.winther@protonmail.com>
2024-05-16 06:45:37 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div>
+				<Image
+					className="py-8"
+					src="/heroImage.png"
+					alt="logo"
+					width={500}
+					height={500}
+				/>
 				<Input type="text" placeholder="Add a book" />
 			</div>
 		</main>
